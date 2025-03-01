1a8a66c2d64fc8200bab2c47fdf835c8232aaed1
ELisanna <elisannamartinezsanchez@gmail.com>
2025-03-01 00:12:36 -0400

    Creando al jugador

--- main.py
+++ main.py
@@ -4,15 +4,31 @@ import sys
 #Iniciando Pygame
 pygame.init()
 
+# Configuración de la pantalla
 ALTO_PANTALLA = 600
 ANCHO_PANTALLA = 800
-
 pantalla = pygame.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA))
 pygame.display.set_caption("Berzerk")
 
+# Colores
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
 ROJO = (255, 0, 0)
+
+#Clase para el jugador
+class Jugador:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.ancho = 32
+        self.alto = 32
+        self.velocidad = 5
+        self.cuadrado = pygame.Rect(x, y, self.ancho, self.alto)
+    
+    def dibujar(self, surface):
+        pygame.draw.rect(surface, ROJO, self.cuadrado)
+
+jugador = Jugador(ANCHO_PANTALLA //2, ALTO_PANTALLA // 2)
 
 # Bucle principal 
 ejecutando = True
@@ -20,3 +36,9 @@ while ejecutando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             ejecutando = False
+    
+    #Dibujar al jugador
+    jugador.dibujar(pantalla)
+
+    #Actualizar la pantalla
+    pygame.display.flip()
